82ab3ddfdf12681b7a3a992defc7c52032dfc877
Rena Jair <164107426+Rene523@users.noreply.github.com>
2025-04-16 21:18:15 +0000

    Final Commit

--- CollegeStudent.java
+++ CollegeStudent.java
@@ -1,10 +1,29 @@
-public class CollegeStudent {
+public class CollegeStudent extends Student{
     private String myMajor;
     private int myYear;
-}
+
 
 public CollegeStudent(String myName, int myAge, String myGender, String myIdNum, double myGPA, int year, String major){
     super(myName, myAge, myGender, myIdNum, myGPA);
     myMajor = major;
     myYear = year;
+}
+
+public int getYear(){
+    return myYear;
+}
+public void setYear(int year){
+    myYear=year;
+}
+public String getMajor(){
+    return myMajor;
+}
+public void setMajor(String major){
+    myMajor=major;
+}
+
+public String toString(){
+    return super.toString() + ", College Student Year : " + myYear + ", major: " + myMajor;
+}
+
 }
--- Teacher.java
+++ Teacher.java
@@ -3,19 +3,16 @@ public class Teacher extends Person{
     // TODO: Add fields.
     private String mySubject;
     private double mySalary;
-
     public Teacher (String myName, int myAge, String myGender, String subject, double salary){
         super(myName, myAge, myGender);
         mySubject = subject;
         mySalary = salary;
     }
-
     public Teacher(String name, int age, String gender)
     {
         super(name, age, gender);
         
     }
-
     public String getSubject(){
         return mySubject;
     }
@@ -28,11 +25,7 @@ public class Teacher extends Person{
     public void setSalary(double salary){
         mySalary=salary;
     }
-
-    
-
     // TODO Add methods
-
     public String toString(){
         // TODO: improve this.
         return super.toString() + ", Teacher subject: " + mySubject + ", salary: " + mySalary;
